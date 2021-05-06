e9f0df633a6f429a60c2275be0134f299b0c422a
Aadi <as808780@gmail.com>
2021-05-06 19:14:18 +0530

    added logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,40 +3,48 @@ import { Button } from "@chakra-ui/button";
 import { Flex } from "@chakra-ui/layout";
 import { Text, useColorMode } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { FaHandHoldingHeart, FaHandsHelping, VscOrganization } from "react-icons/all";
+import {
+  FaHandHoldingHeart,
+  FaHandsHelping,
+  VscOrganization,
+} from "react-icons/all";
 import { Link } from "react-router-dom";
 import "./index.css";
 
-
+import Logo from '../../assets/images/logo192.png'
 
 const Navbar = () => {
   const [auth, setauth] = useState(false);
   const [selectedMenu, setSelectedMenu] = useState();
   const { colorMode, toggleColorMode } = useColorMode();
 
-
   const NavbarButton = (props) => {
     return (
       <Link onClick={() => setSelectedMenu(props.number)} to={props.to}>
         <Button
           size="md"
-          colorScheme={selectedMenu === props.number ? 'messenger' : "gray"}
+          colorScheme={selectedMenu === props.number ? "messenger" : "gray"}
           m={2}
-          h={'4rem'}
-          w={'8rem'}
-          _hover={{ bg: '#2d88ff' }}
+          h={"4rem"}
+          w={"8rem"}
+          _hover={{ bg: "#2d88ff" }}
         >
-          <Flex direction={"column"} alignItems={"center"} justifyContent={'center'} m={2} >
-            <props.icon size={'1.5rem'} />
+          <Flex
+            direction={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            m={2}
+          >
+            <props.icon size={"1.5rem"} />
             <Text>{props.name}</Text>
           </Flex>
         </Button>
       </Link>
     );
-  }
+  };
 
   return (
-    <div className="navbar" >
+    <div className="navbar">
       <Flex
         h="88"
         bg="whiteAlpha.800"
@@ -48,19 +56,37 @@ const Navbar = () => {
         color="black"
       >
         <Flex flex="1">
-          <Avatar
+          <img
             onClick={toggleColorMode}
-            ml={2}
-            name="Logo"
-            src="https://www.statnews.com/wp-content/uploads/2020/02/Coronavirus-CDC-645x645.jpg"
+            alt="logo"
+            style={{height: 60}}
+            src={Logo}
           />
         </Flex>
-        <Flex justifyContent={"space-between"} flex={["1","2","2"]} w={['100%']} maxW="700px">
-         
-         
-          <NavbarButton icon={FaHandsHelping} number="0" name="Find Help" to="/help" />
-          <NavbarButton icon={FaHandHoldingHeart} number="1" name="Provide Help" to="/provide-help" />
-          <NavbarButton icon={VscOrganization} number="2" name="Organizations" to="/organization" />
+        <Flex
+          justifyContent={"space-between"}
+          flex={["1", "2", "2"]}
+          w={["100%"]}
+          maxW="700px"
+        >
+          <NavbarButton
+            icon={FaHandsHelping}
+            number="0"
+            name="Find Help"
+            to="/help"
+          />
+          <NavbarButton
+            icon={FaHandHoldingHeart}
+            number="1"
+            name="Provide Help"
+            to="/provide-help"
+          />
+          <NavbarButton
+            icon={VscOrganization}
+            number="2"
+            name="Organizations"
+            to="/organization"
+          />
 
           {/* <Button
             m={2}
@@ -73,8 +99,6 @@ const Navbar = () => {
             Test Auths
 
           </Button> */}
-
-
         </Flex>
 
         <Flex flex="1" flexDir="row-reverse">
