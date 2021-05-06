de5cf4e6f567bb3449d43db6e035bc579153df89
Aadi <as808780@gmail.com>
2021-05-06 23:16:09 +0530

    added service worker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import store from './store'
 import { Provider } from 'react-redux'
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
 import theme from "./theme"
+import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   // <React.StrictMode>
@@ -27,3 +29,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
