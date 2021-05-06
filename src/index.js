5e2a25c83705d392d643bfea60965cb3d317ba6b
Aadi <as808780@gmail.com>
2021-05-06 23:40:13 +0530

    local service worker

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import store from './store'
 import { Provider } from 'react-redux'
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
 import theme from "./theme"
-import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
@@ -29,4 +28,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
