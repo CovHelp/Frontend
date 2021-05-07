5aa414e900e38f6d0cfbdd5fb65402881c2dcb11
Sidm9 <siddharthm1999@outlook.com>
2021-05-07 23:39:06 +0530

    Fixed Profile Settings

--- src/components/profile/ProfileSetting.js
+++ src/components/profile/ProfileSetting.js
@@ -1,86 +1,90 @@
-import React from 'react'
-import { Box, Checkbox, Link, Stack, Text } from "@chakra-ui/react";
-import { Avatar} from "@chakra-ui/react"
-import { Divider } from "@chakra-ui/react"
+import { Avatar, Box, Divider, Flex, Link, Text } from "@chakra-ui/react";
+import React from 'react';
 import {
-     AiOutlineHome,
-    BiEdit,
-    AiOutlineLock,
-    AiOutlineBell,
-    GrShieldSecurity,
-    } from "react-icons/all";
-    import './Profile.css'
+    AiOutlineHome,
+
+    AiOutlineLock, BiEdit
+} from "react-icons/all";
+import CardBox from '../Cards/CardBox';
+import './Profile.css';
 
 function ProfileSetting() {
     return (
-        <div class="settings__container">
-             
-             <Box pl="16px"  mb="16px" mt="16px">
+        <Box
+            w="100%"
+            p={3}
+            pt={[1, 3]}
+            d="flex"
+            flexDir={"column"}
+            alignItems={"center"}
+            background="#f0f2f5"
+        >
 
-             <Avatar  height="5rem" width="5rem" src="https://bit.ly/broken-link" />
-             </Box>
-        <Box pl="16px" width="100%"  display="flex" flexDirection="column"  >
-                 <Box pl="0px" my="16px" display="flex" alignItems="center" justifyContent="space-between">
-                     <Box display="flex" >
+            <CardBox>
+                <Box background="white" border={"1px"} borderColor="white">
+                    <Box m={[4, 8]} >
 
-                     <AiOutlineHome size="24px"/>
-                     <Link to="">
-                     <Text fontSize="md" ml="24px" fontWeight="500">
-                         Account preview
+                        <Avatar height="5rem" w="5rem" src="https://bit.ly/broken-link" />
+                    </Box>
+                    <Flex px={[4, 8]} w="100%" flexDir="column">
+                        <Flex pl="0px" my="16px" alignItems="center" justifyContent="space-between">
+                            <Flex >
+
+                                <AiOutlineHome size="24px" />
+                                <Link to="">
+                                    <Text fontSize="md" ml="24px" fontWeight="500">
+                                        Account preview
                          </Text>
-                     </Link>
-                     </Box>
-                     <Box  class="arrow__container"  pr="16px">
-                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> 
-                   </svg>
-                     </Box>
-                 </Box>
-                 <Divider/>
-                 <Box  pl="0px" my="16px" display="flex" alignItems="center"  justifyContent="space-between">
-                     <Box display="flex">
+                                </Link>
+                            </Flex>
+                            <Box class="arrow__container" pr={2}>
+                                <svg w="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9 18L15 12L9 6" stroke="black" stroke-w="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </Box>
+                        </Flex>
+                        <Divider />
+                        <Flex pl="0px" my="16px" alignItems="center" justifyContent="space-between">
+                            <Flex >
 
-                     <BiEdit size="24px"/>
-                     <Link to="">
+                                <BiEdit size="24px" />
+                                <Link to="">
 
-                     <Text fontSize="md" ml="24px" fontWeight="500">
-                        Edit Profile
+                                    <Text fontSize="md" ml="24px" fontWeight="500">
+                                        Edit Profile
                          </Text>
-                     </Link>
-                     </Box>
-                     <Box class="arrow__container" pr="16px">
-                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> 
-                   </svg>
-                     </Box>
-                 </Box>
-                 <Divider/>
-                 <Box  pl="0px" my="16px" display="flex" alignItems="center" justifyContent="space-between">
-                     <Box display="flex">
+                                </Link>
+                            </Flex>
+                            <Box class="arrow__container" pr={2}>
+                                <svg w="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9 18L15 12L9 6" stroke="black" stroke-w="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </Box>
+                        </Flex>
+                        <Divider />
+                        <Flex pl="0px" my="16px" alignItems="center" justifyContent="space-between">
+                            <Flex >
 
-                     <AiOutlineLock size="24px"/>
-                     <Link to="">
+                                <AiOutlineLock size="24px" />
+                                <Link to="">
 
-                     <Text fontSize="md" ml="24px" fontWeight="500">
-                         Change Password
+                                    <Text fontSize="md" ml="24px" fontWeight="500">
+                                        Change Password
                          </Text>
-                     </Link>
-                     </Box>
-                     <Box class="arrow__container"  pr="16px">
-                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> 
-                   </svg>
-                     </Box>
-                 </Box>
-                 <Divider/>
-                 
-                 
-                
-                 
+                                </Link>
+                            </Flex>
+                            <Box class="arrow__container" pr={2}>
+                                <svg w="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9 18L15 12L9 6" stroke="black" stroke-w="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </Box>
+                        </Flex>
+                        <Divider mb={[5, 10]} />
+                    </Flex>
+                </Box>
+            </CardBox>
 
-
-             </Box>
-             </div>
+        </Box>
     )
 }
 
@@ -98,12 +102,12 @@ export default ProfileSetting
     </Link>
     </Box>
     <Box class="arrow__container"  pr="16px">
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> 
+    <svg w="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9 18L15 12L9 6" stroke="black" stroke-w="2" stroke-linecap="round" stroke-linejoin="round"/> 
   </svg>
     </Box>
 </Box>
-<Divider/>
+<DividerComponent/>
 <Box  pl="0px" my="16px" display="flex" alignItems="center" justifyContent="space-between">
     <Box display="flex">
 
@@ -115,9 +119,9 @@ export default ProfileSetting
         </Link>
 </Box>
 <Box  class="arrow__container" pr="16px">
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> 
+    <svg w="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9 18L15 12L9 6" stroke="black" stroke-w="2" stroke-linecap="round" stroke-linejoin="round"/> 
   </svg>
     </Box>
     </Box>
-<Divider/> */}
+<DividerComponent/> */}
