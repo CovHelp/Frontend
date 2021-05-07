24425d3a656078d892ec669908342ac6903c07e3
Sidm9 <siddharthm1999@outlook.com>
2021-05-08 01:11:49 +0530

    New Post Merge Conflict

--- src/pages/Newpost/NewPost.js
+++ src/pages/Newpost/NewPost.js
@@ -1,64 +1,113 @@
-import { Box, Button, Checkbox, Flex, FormControl, FormLabel, Input, InputGroup, InputLeftAddon, Select, Stack, Textarea, useColorModeValue } from '@chakra-ui/react';
-import { Slider, SliderFilledTrack, SliderThumb, SliderTrack } from "@chakra-ui/slider";
-import React, { useEffect, useState } from 'react';
-import { BoilerPlateForGiveHelp, BoilerPlateForNeedHelp } from '../../components/newpost/BoilerPlate';
-import StateCitySelctor from '../../components/newpost/StateCitySelector';
+import {
+    Box,
+    Button,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputLeftAddon,
+    Select,
+    Stack,
+    Textarea,
+    useColorModeValue,
+} from "@chakra-ui/react";
+import {
+    Slider,
+    SliderFilledTrack,
+    SliderThumb,
+    SliderTrack,
+} from "@chakra-ui/slider";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { createNeedHelpPost } from "../../api/post";
+import {
+    BoilerPlateForGiveHelp,
+    BoilerPlateForNeedHelp,
+} from "../../components/newpost/BoilerPlate";
+import StateCitySelctor from "../../components/newpost/StateCitySelector";
 
 const NewPost = (props) => {
-
     const [category, setCategory] = useState(0);
-    const [shareNumber, setshareNumber] = useState(true)
-    const [urgencySliderValue, setUrgencySliderValue] = useState(1)
+    const [shareNumber, setshareNumber] = useState(true);
+    const [urgencySliderValue, setUrgencySliderValue] = useState(1);
+    const [phoneNumber, setPhoneNumber] = useState("");
     const [locationSliderValue, setLocationSliderValue] = useState(1)
-    const [phoneNumber, setPhoneNumber] = useState('Enter Number')
-    const [body, setBody] = useState('')
+
+    const [body, setBody] = useState("");
+    const [city, setCity] = useState({});
+    const [state, setState] = useState({});
+
+    const userStore = useSelector(store => store.userStore);
+
     const handleCategoryChange = (e) => {
         setCategory(e.target.value - 1);
 
-        setBody(props.typeOfPost === "Request Help" ? BoilerPlateForNeedHelp[e.target.value - 1] : BoilerPlateForGiveHelp[e.target.value - 1]);
-    }
-    const handleUrgencySliderChange = (val) => setUrgencySliderValue(val);
+        setBody(
+            props.typeOfPost === "Request Help"
+                ? BoilerPlateForNeedHelp[e.target.value - 1]
+                : BoilerPlateForGiveHelp[e.target.value - 1]
+        );
+    };
     const handleLocationSliderChange = (val) => {setLocationSliderValue(val)};
+    const handleUrgencySLiderChange = (val) => setUrgencySliderValue(val);
     const handlePhoneNumberChange = (e) => setPhoneNumber(e.target.value);
     const handleSetBody = (e) => setBody(e.target.value);
-
-
-    useEffect(() => {
-        console.log(props.typeOfPost);
-    }, [])
+    const handleLocationSelection = (city, state) => {
+        console.log(city, state);
+        setState(state);
+        setCity(city)
+    }
 
     // PHONE NUMBER LESS THAN 10
     const handleIncorrectNumber = () => {
-        var phoneNumberResponse = phoneNumber.toLocaleString.length < 10 ? false : true
-        return phoneNumberResponse
+        var phoneNumberResponse =
+            phoneNumber.toLocaleString.length < 10 ? false : true;
+        return phoneNumberResponse;
+    };
+
+    const handleCreatePost = async () => {
+        if (props.typeOfPost === 'Request Help') {
+            try {
+                await createNeedHelpPost({
+                    body: body,
+                    category: category,
+                    phoneNumber: phoneNumber,
+                    isPhoneNumberPublic: shareNumber,
+                    picture: '',
+                    urgency: urgencySliderValue,
+                    city: city.name,
+                    state: state.name,
+                    lat: city.latitude,
+                    long: city.longitude,
+                    token: userStore.token.token
+                })
+            } catch (e) {
+                console.log("Error")
+            }
+        } else {
+
+        }
     }
 
     return (
-        <Flex
-            minH={'50vh'}
-            align={'center'}
-            justifyContent={'center'}>
-            <Stack width={'md'}>
-                <Box
-                    rounded={'lg'}
-                    bg={useColorModeValue('white', 'gray.700')}
-
-                >
-
+        <Flex minH={"50vh"} align={"center"} justifyContent={"center"}>
+            <Stack width={"md"}>
+                <Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")}>
                     <Stack spacing={4} my={2}>
-
                         {/* <span>bitchh {category} |
-                       urgency {sliderValue} | phone {phoneNumber} | body {body}</span> */}
+                         urgency {urgencySliderValue} | phone {phoneNumber} | body {body}</span> */}
 
                         <FormControl id="Category">
-                            <FormLabel>Select Cateogry</FormLabel>
+                            <FormLabel>{props.typeOfPost}</FormLabel>
 
                             <Select
                                 isRequired
                                 placeholder="Select Category"
-                                border={'2px'}
-                                onChange={(e) => handleCategoryChange(e)}>
-
+                                border={"2px"}
+                                onChange={(e) => handleCategoryChange(e)}
+                            >
                                 <option value="1">Oxygen</option>
                                 <option value="2">Ambulance</option>
                                 <option value="3">Medicine</option>
@@ -70,12 +119,13 @@ const NewPost = (props) => {
 
                         <FormControl id="Data">
                             <FormLabel>Enter Details</FormLabel>
-                            <Textarea border={'2px'}
+                            <Textarea
+                                border={"2px"}
                                 height="auto"
                                 value={body}
-                                onChange={(e) => handleSetBody(e)} />
+                                onChange={(e) => handleSetBody(e)}
+                            />
                         </FormControl>
-
 
 
 
@@ -83,7 +133,7 @@ const NewPost = (props) => {
                             <Flex direction="column" m={2}>
                                 <FormLabel>
                                     Select Number of Locations
-                                </FormLabel>
+                                  </FormLabel>
 
                                 <Slider flex="1" min={1} max={5}
                                     focusThumbOnChange={false}
@@ -99,71 +149,72 @@ const NewPost = (props) => {
                             </Flex>
                         }
 
-                        <StateCitySelctor/>
 
-                        {props.typeOfPost === "Request Help" &&
+                        <StateCitySelctor onSelected={handleLocationSelection} />
+                        {props.typeOfPost}
 
-
+                        {props.typeOfPost === "Request Help" && (
                             <Flex direction="column" m={2}>
+                                <FormLabel>Urgency level</FormLabel>
 
-                                <FormLabel>
-                                    Urgency level
-                            </FormLabel>
-
-                                <Slider flex="1" min={1} max={3}
+                                <Slider
+                                    flex="1"
+                                    min={1}
+                                    max={3}
                                     focusThumbOnChange={false}
-                                    sliderValue={urgencySliderValue}
+                                    SliderValue={urgencySliderValue}
                                     defaultValue={0}
-                                    onChange={handleUrgencySliderChange}>
-
+                                    onChange={handleUrgencySLiderChange}
+                                >
                                     <SliderTrack>
-                                        <SliderFilledTrack defaultValue={0} bg={urgencySliderValue === 2 ? 'orange' : 'red'} />
+                                        <SliderFilledTrack
+                                            defaultValue={0}
+                                            bg={urgencySliderValue === 2 ? "orange" : "red"}
+                                        />
                                     </SliderTrack>
-                                    <SliderThumb fontSize="sm" boxSize="32px" children={urgencySliderValue} />
+                                    <SliderThumb
+                                        fontSize="sm"
+                                        boxSize="32px"
+                                        children={urgencySliderValue}
+                                    />
                                 </Slider>
                             </Flex>
-
-
-                        }
-                        <Checkbox pt={2} outline="none"
+                        )}
+                        <Checkbox
+                            pt={2}
+                            outline="none"
                             defaultIsChecked
-                            onChange={() => setshareNumber(!shareNumber)}>
-                            Share My Phone Number</Checkbox>
+                            onChange={() => setshareNumber(!shareNumber)}
+                        >
+                            Share My Phone Number
+              </Checkbox>
 
-
-
-                        {shareNumber &&
+                        {shareNumber && (
                             <FormControl id="phone Number">
-                                <FormLabel>
-                                    Phone Number
-                            </FormLabel>
+                                <FormLabel>Phone Number</FormLabel>
 
                                 <InputGroup mb={4}>
                                     <InputLeftAddon children="+91" />
 
                                     <Input
-                                        border={'2px'}
+                                        border={"2px"}
                                         type="number"
                                         value={phoneNumber}
                                         onChange={(e) => handlePhoneNumberChange(e)}
                                         disabled={!shareNumber}
-                                        placeholder={'Enter Number'} />
+                                        placeholder={"Enter Number"}
+                                    />
                                 </InputGroup>
                             </FormControl>
-                        }
+                        )}
 
-                        <Button
-                            bg="messenger.500"
-                            color={'white'}
-                            borderRadius="lg">
+                        <Button onClick={handleCreatePost} bg="messenger.500" color={"white"} borderRadius="lg">
                             Post
-                        </Button>
-
+              </Button>
                     </Stack>
-
                 </Box>
             </Stack>
-        </Flex >
-    )
-}
+        </Flex>
+    );
+};
 export default NewPost;
