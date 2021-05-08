fa5e466100f28953fdb31f980a5f7964e5f24fc7
Sidm9 <siddharthm1999@outlook.com>
2021-05-08 23:36:05 +0530

    Comment Bubbles

--- src/components/CommentBubble/CommentBubble.jsx
+++ src/components/CommentBubble/CommentBubble.jsx
@@ -6,6 +6,16 @@ const CommentBubble = (props) => {
     return (
         <>
 
+            <Flex my={1} flexDir="column" bgColor="rgb(245,245,245)" borderRadius="lg" maxW={["100%", "95%"]} h="auto" p={[2, 3]}>
+                <Flex alignItems="center">
+                    <Avatar w={["20px", "28px"]}
+                        h={["20px", "28px"]}
+                        mr={2} />
+                    <Heading fontSize="md">{props.name}</Heading>
+                </Flex>
+                <Text noOfLines={[4, 5, 6]} mt={2}>{props.comment}</Text>
+
+            </Flex>
         </>
     )
 }
