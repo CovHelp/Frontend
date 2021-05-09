8cd37d83b1d3ae46a2510292bcc91bea05cc88e0
Sidm9 <siddharthm1999@outlook.com>
2021-05-09 14:19:21 +0530

    Organization Card Init

---
+++ src/components/Cards/OrganizationCard/OrganizationCard.jsx
@@ -0,0 +1,24 @@
+import { Box } from '@chakra-ui/layout'
+import React from 'react'
+
+const OrganizationCard = () => {
+    return (
+        <>
+            <Box
+                backgroundColor="white"
+                mt={4}
+                p={["4", "8"]}
+                pb={["6", "10"]}
+                w="100%"
+                h="200px"
+                borderRadius="xl"
+                borderWidth="1px"
+                background="white"
+            >
+                ok
+            </Box>
+        </>
+    )
+}
+
+export default OrganizationCard
--- src/pages/Organization/Organization.js
+++ src/pages/Organization/Organization.js
@@ -1,11 +1,39 @@
+
+import { Box, Grid } from '@chakra-ui/layout'
 import React from 'react'
-import MainHeading from '../../components/MainHeading/MainHeading'
+import OrganizationCard from '../../components/Cards/OrganizationCard/OrganizationCard'
 
 const Organization = () => {
     return (
-        <>
-            <MainHeading name="Organizations"/>
-        </>
+
+        <Grid
+            w="100%"
+            border="4px"
+            p={4}
+            justifyContent="center"
+            gap={[2,4,6,8]}
+            templateColumns="repeat(auto-fill,minmax(240px,350px))"
+            background="#f0f2f5"
+        >
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+            <OrganizationCard />
+
+        </Grid>
+
     )
 }
 
