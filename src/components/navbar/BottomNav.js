8247961379b6bd5562180e7e6bf342abbe57c692
Aryan Choudhary <sep2001aryan@yahoo.com>
2021-05-11 00:48:41 +0530

    there is bug in this code in bottomnav.js

--- src/components/navbar/BottomNav.js
+++ src/components/navbar/BottomNav.js
@@ -1,79 +1,227 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { BiChat } from "react-icons/bi";
 import { FaHandHoldingHeart, FaHandsHelping } from "react-icons/fa";
 import { VscAccount, VscOrganization } from "react-icons/vsc";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./index.css";
+import { useDispatch } from "react-redux";
 
 const NavItem = ({
   Icon,
-  index,
   activeIndex,
-  handleIndexCallback,
   to,
   text,
 }) => {
   return (
     <Link
-      className={index === activeIndex ? "activeButton" : "btn"}
+      className={activeIndex ? "activeButton" : "btn"}
       to={to}
-      onClick={() => handleIndexCallback(index)}
     >
-      <Icon color={activeIndex === index ? "#0078ff" : "#4f5662"} size={20} />
-      {activeIndex === index && <p>{text}</p>}
+      <Icon color={activeIndex ? "#0078ff" : "#4f5662"} size={20} />
+      {activeIndex && <p>{text}</p>}
     </Link>
   );
 };
 
 export default function BottomNav() {
   const userStore = useSelector((store) => store.userStore);
+  const dispatch = useDispatch();
+  const navState = useSelector((store) => store.navState);
+  const location = useLocation();
+
+  useEffect(() => {
+    // console.log(location.pathname);
+    if(location.pathname === '/'){
+      let navState = {
+        getHelp: true,
+        provideHelp: false,
+        oganization: false,
+        chat: false,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    } else if(location.pathname === '/provide-help'){
+      let navState = {
+        getHelp: false,
+        provideHelp: true,
+        oganization: false,
+        chat: false,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    } else if(location.pathname === '/organization'){
+      let navState = {
+        getHelp: false,
+        provideHelp: false,
+        oganization: true,
+        chat: false,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    }else if(location.pathname === '/chat'){
+      let navState = {
+        getHelp: false,
+        provideHelp: false,
+        oganization: false,
+        chat: true,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    } else if(location.pathname === '/profile'){
+      let navState = {
+        getHelp: false,
+        provideHelp: false,
+        oganization: false,
+        chat: false,
+        profile: true,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    }
+    // eslint-disable-next-line
+  }, [location])
+
+
+  useEffect(() => {
+    console.log(location.pathname);
+    if(location.pathname === '/'){
+      let navState = {
+        getHelp: true,
+        provideHelp: false,
+        oganization: false,
+        chat: false,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    } else if(location.pathname === '/provide-help'){
+      let navState = {
+        getHelp: false,
+        provideHelp: true,
+        oganization: false,
+        chat: false,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    } else if(location.pathname === '/organization'){
+      let navState = {
+        getHelp: false,
+        provideHelp: false,
+        oganization: true,
+        chat: false,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    }else if(location.pathname === '/chat'){
+      let navState = {
+        getHelp: false,
+        provideHelp: false,
+        oganization: false,
+        chat: true,
+        profile: false,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    } else if(location.pathname === '/profile'){
+      let navState = {
+        getHelp: false,
+        provideHelp: false,
+        oganization: false,
+        chat: false,
+        profile: true,
+      }
+
+      dispatch({
+        type: 'UPDATE_NAV',
+        payload: navState
+      })
+    }
+    // eslint-disable-next-line
+  }, [])
+
+  useEffect(() => {
+    console.log(navState)
+    // eslint-disable-next-line
+  }, [location])
 
   const items = [
-    { icon: FaHandsHelping, index: 0, to: "/", text: "Get help" },
+    { icon: FaHandsHelping, index: 0, to: "/", text: "Get help", activeValue: navState.getHelp },
     {
       icon: FaHandHoldingHeart,
       index: 1,
       to: "/provide-help",
       text: "Give help",
+      activeValue: navState.provideHelp,
     },
     {
       icon: VscOrganization,
       index: 2,
       to: "/organization",
       text: "Organizations",
+      activeValue: navState.oganization,
     },
-    { icon: BiChat, index: 3, to: "/chat", text: "Chat" },
+    { icon: BiChat, index: 3, to: "/chat", text: "Chat", activeValue: navState.chat },
     {
       icon: VscAccount,
       index: 4,
       to: "/profile",
       text: "Profile",
-      onClick: { handleClick },
+      activeValue: navState.profile,
     },
   ];
 
-  const unAuthItems = [
-    { icon: FaHandsHelping, index: 0, to: "/", text: "Get help" },
+  const unAuthItems =[
+    { icon: FaHandsHelping, index: 0, to: "/", text: "Get help", activeValue: navState.getHelp },
     {
       icon: FaHandHoldingHeart,
       index: 1,
       to: "/provide-help",
       text: "Give help",
+      activeValue: navState.provideHelp,
     },
     {
       icon: VscOrganization,
       index: 2,
       to: "/organization",
       text: "Organizations",
+      activeValue: navState.organization,
     },
   ];
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const handleNavIndex = (index) => {
-    setActiveIndex(index);
-  };
 
   function handleClick() {
     console.log("hi");
@@ -83,30 +231,26 @@ export default function BottomNav() {
     <div className="bottom-nav">
       {userStore.token ? (
         <div className="bottom-nav-grid">
-          {items.map((item, index) => (
+          {items.map((item) => (
             <NavItem
-              key={index}
+              key={item.index}
               to={item.to}
               text={item.text}
               Icon={item.icon}
-              index={item.index}
-              activeIndex={activeIndex}
-              handleIndexCallback={handleNavIndex}
+              activeIndex={item.activeValue}
               onClick={handleClick}
             />
           ))}
         </div>
       ) : (
         <div className="bottom-nav-grid-unAuth">
-          {unAuthItems.map((item, index) => (
+          {unAuthItems.map((item) => (
             <NavItem
-              key={index}
+              key={item.index}
               to={item.to}
               text={item.text}
               Icon={item.icon}
-              index={item.index}
-              activeIndex={activeIndex}
-              handleIndexCallback={handleNavIndex}
+              activeIndex={item.activeValue}
               onClick={handleClick}
             />
           ))}
