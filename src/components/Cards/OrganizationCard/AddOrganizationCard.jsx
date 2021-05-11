ef1311c25306f86189ffb05506cd88c497d7cca8
Sidm9 <siddharthm1999@outlook.com>
2021-05-11 20:52:01 +0530

    Organization Card init

--- src/components/Cards/OrganizationCard/AddOrganizationCard.jsx
+++ src/components/Cards/OrganizationCard/AddOrganizationCard.jsx
@@ -1,15 +1,19 @@
-import { Flex } from '@chakra-ui/layout'
+import { Flex, Heading } from '@chakra-ui/layout'
 import React from 'react'
 import { BsPlusCircle } from 'react-icons/bs'
 const AddOrganizationCard = () => {
     return (
         <>
-            <Flex p={["4", "8"]} bgColor="white" borderRadius="xl" h={"400px"}>
-                <Flex alignItems="center" justifyContent="center" flexDir="column" _dark=" true" w="100%" minH="400px" maxH="700px">
+            <Flex p={3} bgColor="white" borderRadius="xl" h={"400px"} border="3px dashed">
+                <Flex alignItems="center" justifyContent="center" flexDir="column" _dark=" true" w="100%" minH="400px" maxH="700px" textAlign="center"
+                    _hover={{
+                        transform: "scale(1.2)"
+                    }}>
 
                     <BsPlusCircle fontSize="60px" />
 
-
+                    <br /><br />
+                    <Heading as={"h3"} fontSize="lg">Add a new Organization</Heading>
                 </Flex>
             </Flex>
         </>
